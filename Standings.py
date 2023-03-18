f7ba49176dbe0b4c9e80c62598d527d6911e0ff0
Firnaz Ahamed <firnazahamed@gmail.com>
2023-03-18 15:21:42 -0700

    Update main page

--- Standings.py
+++ Standings.py
@@ -3,8 +3,9 @@ import pandas as pd
 from helpers import read_file
 
 st.set_page_config(layout="wide")
-st.title("Cric Talk Draft")
-st.markdown("Summer Is Coming 2023")
+st.title("Summer Is Coming 2023")
+st.subheader("**:blue[Cric Talk Draft]** :fire:")
+
 
 bucket_name = "summer-is-coming-2023"
 standings_file_path = "Outputs/standings_df.csv"
@@ -14,7 +15,7 @@ standings_df = read_file(bucket_name, standings_file_path).set_index("Standings"
 cumsum_df = read_file(bucket_name, cumsum_file_path).set_index("Owner")
 col1, col2 = st.columns([2, 3])
 
-col1.subheader("Standings")
+col1.subheader("Standings ")
 col1.dataframe(standings_df)
 
 col2.subheader("Draft Standings Race")
