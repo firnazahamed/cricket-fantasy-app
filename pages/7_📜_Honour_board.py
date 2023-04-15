521ee6763718ecd49a87500c1791c7dcbd2e45b4
Firnaz Ahamed <firnazahamed@gmail.com>
2023-04-14 17:32:13 -0700

    Add honour board

---
+++ pages/7_📜_Honour_board.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+from helpers import read_file
+
+st.set_page_config(layout="wide")
+honour_board = pd.DataFrame(
+    {
+        "Season": [2019, 2020, 2021, 2022],
+        "Winner": ["Mabbu", "Saju", "Saju", "Firi"],
+        "Runner-up": ["Bhar", "Siddhu", "Srini", "Bhar"],
+        "Second runner-up": ["Shar", "Srini", "Firi", "Saju"],
+    }
+).set_index("Season")
+
+st.header("Honour Board")
+st.table(honour_board)
+
+st.header("Past Seasons")
+bucket_name = "summer-is-coming-2023"
+
+for year in [2022, 2021, 2020, 2019]:
+
+    col1, col2 = st.columns([2, 3])
+
+    standings_df = read_file(bucket_name, f"Seasons/{year}_standings.csv").set_index(
+        "Standings"
+    )
+    col1.subheader(f"{year} Final Standings")
+    col1.dataframe(standings_df)
+
+    cumsum_df = read_file(bucket_name, f"Seasons/{year}_cumsum.csv").set_index("Owner")
+    cumsum_df = cumsum_df.rename(columns={x: int(x) for x in cumsum_df.columns})
+    col2.subheader(f"{year} Standings Race")
+    col2.line_chart(cumsum_df.T)
+
+    st.markdown("#")
